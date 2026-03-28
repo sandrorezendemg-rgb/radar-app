f21ec7648d57fa247e3099a1f2faf6ca1e4290b6
sandrorezendemg-rgb <sandro.rezendemg@gmail.com>
2026-03-28 01:38:36 -0300

    Atualizar o radar_engine.py

--- radar_engine.py
+++ radar_engine.py
@@ -3,8 +3,19 @@ import pandas as pd
 def run_radar(narratives, risk, mode):
 
     data = [
-        {"Ativo": "RNDR", "Score": 6, "Sinal": "COMPRA"},
-        {"Ativo": "INJ", "Score": 5, "Sinal": "COMPRA"},
+        {
+            "Ativo": "RNDRUSDT",
+            "Entrada": 7.42,
+            "SL": 7.27,
+            "TP1": 7.57,
+            "TP2": 7.71,
+            "Score": 6,
+            "Sinal": "COMPRA",
+            "1D": "Alta (BOS)",
+            "4H": "OB + FVG",
+            "15M": "Sweep + HL",
+            "1M": "Confirmação"
+        }
     ]
 
     return pd.DataFrame(data)
