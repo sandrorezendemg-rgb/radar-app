b7d9c2fcbb70f4101c53e82ac96bd627e187cf69
sandrorezendemg-rgb <sandro.rezendemg@gmail.com>
2026-03-28 01:09:41 -0300

    Criar radar_engine.py

---
+++ radar_engine.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def run_radar(narratives, risk, mode):
+
+    data = [
+        {"Ativo": "RNDR", "Score": 6, "Sinal": "COMPRA"},
+        {"Ativo": "INJ", "Score": 5, "Sinal": "COMPRA"},
+    ]
+
+    return pd.DataFrame(data)
